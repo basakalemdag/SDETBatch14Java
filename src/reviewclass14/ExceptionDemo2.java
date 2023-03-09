b91771219c44fab3da6cca1097bbc2d228cca813
basakalemdag <basakalemdag@gmail.com>
2023-03-10 02:41:30 +0300

    Sdet Batch 14 Java Classes 11-30

---
+++ src/reviewclass14/ExceptionDemo2.java
@@ -0,0 +1,13 @@
+package reviewclass14;
+
+import utils.ExcelReader;
+
+public class ExceptionDemo2 {
+    public static void main(String[] args)  {
+
+
+        ExcelReader.read("dkjbfkjdbf");
+
+
+    }
+}
